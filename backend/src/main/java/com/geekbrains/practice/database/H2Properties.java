5df8e0a9292c11c68d44490bc1a7f2f97a3561f0
Egor Klepikov <Egor.Klepikov@jetbrains.com>
2021-01-31 17:11:42 +0300

    Updated H2 credentials

--- backend/src/main/java/com/geekbrains/practice/database/H2Properties.java
+++ backend/src/main/java/com/geekbrains/practice/database/H2Properties.java
@@ -1,7 +1,7 @@
 package com.geekbrains.practice.database;
 
 public class H2Properties {
-  public static final String DB_URL = "jdbc:h2:~/test";
+  public static final String DB_URL = "jdbc:h2:~/chat";
   public static final String LOGIN = "sa";
   public static final String PASSWORD = "";
 }
